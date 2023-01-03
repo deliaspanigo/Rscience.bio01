6edb1cff4814d68ab17bc9ea237f96f66f644d44
deliaspanigo <d.eliaspanigo@gmail.com>
2023-01-03 14:58:37 -0300

    Second Commit

---
+++ 000_Instalacion.R
@@ -0,0 +1,6 @@
+library(devtools)
+install_github("deliaspanigo/Rscience.base", force = TRUE)
+install_github("deliaspanigo/Rscience.bio01", force = TRUE)
+
+library("Rscience.base")
+library("Rscience.bio01")
